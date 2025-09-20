c7c1a3152383be1a5675bdd528db965226f58ed6
shrook <shrookabdelmonem55@gmail.com>
2025-09-20 21:07:39 +0300

    +[feat]: added providers logic

--- lib/screens/profileTab.dart
+++ lib/screens/profileTab.dart
@@ -1,14 +1,26 @@
-import 'package:evently_project/screens/widgets/navBarWidget.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:evently_project/providers/appLanguageProvider.dart';
 import 'package:evently_project/utils/appColors.dart';
 import 'package:evently_project/utils/appAssets.dart';
+import 'package:evently_project/utils/appStyles.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ProfileTab extends StatelessWidget {
+import '../providers/appThemeProvider.dart';
 
+class ProfileTab extends StatefulWidget {
+
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     var width=MediaQuery.of(context).size.width;
     var height=MediaQuery.of(context).size.height;
+    var languageProvider = Provider.of<AppLanguageProvider>(context);
+    var themeProvider = Provider.of<AppThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
@@ -34,12 +46,90 @@ class ProfileTab extends StatelessWidget {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add, size: 40, color: AppColors.offWhiteColor,),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: height*0.028,horizontal: width*0.04),
+        child: Column(spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('language',style: Theme.of(context).textTheme.bodyMedium,).tr(),
+            buildDropdown(value: languageProvider.appLanguage,
+                items: ['en', 'ar'],
+                labelBuilder: (code) {
+                  if (code == 'en') return tr('english');
+                  if (code == 'ar') return tr('arabic');
+                  return code;
+                },
+                onChanged: (String? value) {
+                  if (value != null) {
+                    languageProvider.changeLanguage(context, value);
+                  }
+                }
+            ),
+            Text('theme',style: Theme.of(context).textTheme.bodyMedium,).tr(),
+            buildDropdown(value: themeProvider.appTheme,
+              items: ['light', 'dark'],
+              labelBuilder: (code) {
+                if (code == 'light') return tr('light');
+                if (code == 'dark') return tr('dark');
+                return code;
+              },
+              onChanged: (String? value) {
+                if (value != null) {
+                  themeProvider.changeTheme(value);
+                }
+              },
+            ),
+            Spacer(),
+            ElevatedButton(
+              onPressed: (){}, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.redColor,
+                padding: EdgeInsets.all(16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(16),
+                )
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.logout,color: AppColors.whiteColor,size: 24,),
+                  SizedBox(width: 13,),
+                  Text('logout',style: AppStyles.regular20white,).tr(),
+                ],
+              )
+            )
+          ],
+        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: NavBarWidget(),
+    );
+  }
+
+  Widget buildDropdown({required String value,
+    required List<String> items,
+    required ValueChanged<String?> onChanged,
+    required String Function(String) labelBuilder,}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16,vertical: 6),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.primaryColor, width: 2),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: DropdownButtonFormField<String>(
+        initialValue: value,
+        style: AppStyles.bold20primary,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+        ),
+        items: items.map((code) {
+          return DropdownMenuItem(
+            value: code,
+            child: Text(
+              labelBuilder(code),
+              style: code==value?AppStyles.bold20primary:AppStyles.bold20black,
+            ),
+          );
+        }).toList(),
+        onChanged: onChanged,
+      ),
     );
   }
 }
--- lib/utils/appAssets.dart
+++ lib/utils/appAssets.dart
@@ -1,5 +1,6 @@
 class AppAssets{
   ///introScreen///
+  static const String onBoardingImg = 'assets/images/introAssets/being-creative.png';
   static const String introScreen1 = 'assets/images/introAssets/intro1.png';
   static const String introScreen2 = 'assets/images/introAssets/intro2.png';
   static const String introScreen3 = 'assets/images/introAssets/intro3.png';
@@ -10,6 +11,8 @@ class AppAssets{
   static const String profileImg = 'assets/images/profileTabAssets/profileImg.png';
 
   ///icons///
+  static const String usIcon = 'assets/icons/US.png';
+  static const String egIcon = 'assets/icons/EG.png';
   static const String heartIcon = 'assets/icons/Heart.png';
   static const String homeIcon = 'assets/icons/Home.png';
   static const String mapIcon = 'assets/icons/Map_Pin.png';
